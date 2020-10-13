982adbfef56b1d0194eca4c1af097218ca5a76d1
Kuba <kwiatkowskijakub@protonmail.com>
2020-10-13 13:25:08 +0200

    Initial commit

---
+++ LAB1/zad1.py
@@ -0,0 +1,20 @@
+from cv2 import *
+
+cap = VideoCapture("film/szukaj_zielonego.webm")
+if not cap.isOpened():
+    print("Błąd otwierania pliku")
+
+print("Wymiary: " + str(CAP_PROP_FRAME_WIDTH) + "x" + str(CAP_PROP_FRAME_HEIGHT))
+
+while True:
+    success, img = cap.read()
+    if success:
+        flip(img, +1, img)
+        imshow("Video", img)
+    else:
+        break
+    if waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
