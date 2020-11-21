8123aa0c9e280d5bcf49825c83593dbc7b8ef5da
panjacob <kwiatkowskijakub@protonmail.com>
2020-11-21 21:49:21 +0100

    LAB6

--- LAB6/zad1.py
+++ LAB6/zad1.py
@@ -1,36 +1,35 @@
-import random
-import numpy as np
+from LAB6.utilis import hillClimbDown
+from math import *
+import matplotlib.pyplot as plt
 
 
 def sphere_f(x, n):
     sum = 0
     for i in range(1, n):
-        sum += pow(x, 2)
+        sum += x * x
     return sum
 
 
-def randomGeneratorInt():
-    a = -10000
-    b = 10000
-    return random.randint(a, b)
+def booth_f(x, y):
+    return pow((x + 2 * y - 7), 2) + pow((2 * x + y - 5), 2)
 
 
-def find_neighbours(x, begin,end, accuracy):
-    # linespace
-    neighbours = np.arange(begin,end, accuracy)
-    return neighbours.tolist()
+def mc_cormick_f(x, y):
+    return sin(x + y) + pow(x - y, 2) - 1.5 * x + 2.5 * y + 1
 
 
-def hillClimbRandMin(function, randomGenerator, attempts):
-    lowest = 9999
-    for n in range(0, attempts):
-        current = function(1, n)
-        if current <= lowest:
-            lowest = current
-            print("attepmt: " + str(n) + "  val: " + str(lowest))
-    print(lowest)
+results = []
+for n in range(0, 20):
+    result = hillClimbDown(function=mc_cormick_f, domain_x=(-1.5, 4), domain_y=(-3, 4), ATTEMPTS=1000000,
+                           ACCURACY_FLOAT_POINTS_X=5, ACCURACY_FLOAT_POINTS_Y=5)
+    # result = hillClimbDown(function=booth_f, domain_x=(-10, 10), domain_y=(-10, 10), ATTEMPTS=1000,
+    #                        ACCURACY_FLOAT_POINTS_X=0,
+    #                        ACCURACY_FLOAT_POINTS_Y=0)
 
+    # result = hillClimbDown(function=sphere_f, domain_x=(-1000, 1000), domain_y=(1, 100), ATTEMPTS=100000,
+    #                        ACCURACY_FLOAT_POINTS_X=1, ACCURACY_FLOAT_POINTS_Y=0)
+    results.append(result)
 
-
-x = find_neighbours(10, -5,5, 0.1)
-print(x)
+for result in results:
+    plt.plot(result)
+plt.show()
