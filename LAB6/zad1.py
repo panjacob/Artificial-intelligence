d069913296c62caddf606f1ccb7d9fddac22de28
panjacob <kwiatkowskijakub@protonmail.com>
2020-11-24 13:26:57 +0100

    LAB6 - average

--- LAB6/zad1.py
+++ LAB6/zad1.py
@@ -1,6 +1,21 @@
 from LAB6.utilis import hillClimbDown
+from LAB6.randutilis import hillClimbDown_rand
 from math import *
 import matplotlib.pyplot as plt
+
+
+def avg_plot(results, title):
+    sum = []
+    for result in results:
+        for i, el in enumerate(result):
+            if len(sum) > i:
+                sum[i] += el
+            else:
+                sum.append(el)
+        # plt.plot(result)
+    plt.plot(sum)
+    plt.title(title)
+    plt.show()
 
 
 def sphere_f(x, n):
@@ -18,18 +33,40 @@ def mc_cormick_f(x, y):
     return sin(x + y) + pow(x - y, 2) - 1.5 * x + 2.5 * y + 1
 
 
-results = []
+results_mc_cormick_f = []
+results_booth_f = []
+results_sphere_f = []
 for n in range(0, 20):
-    result = hillClimbDown(function=mc_cormick_f, domain_x=(-1.5, 4), domain_y=(-3, 4), ATTEMPTS=1000000,
-                           ACCURACY_FLOAT_POINTS_X=5, ACCURACY_FLOAT_POINTS_Y=5)
-    # result = hillClimbDown(function=booth_f, domain_x=(-10, 10), domain_y=(-10, 10), ATTEMPTS=1000,
-    #                        ACCURACY_FLOAT_POINTS_X=0,
-    #                        ACCURACY_FLOAT_POINTS_Y=0)
+    results_mc_cormick_f.append(
+        hillClimbDown_rand(function=mc_cormick_f, domain_x=(-1.5, 4), domain_y=(-3, 4), ATTEMPTS=100000,
+                           ACCURACY_FLOAT_POINTS_X=5, ACCURACY_FLOAT_POINTS_Y=5))
+    results_booth_f.append(hillClimbDown_rand(function=booth_f, domain_x=(-10, 10), domain_y=(-10, 10), ATTEMPTS=1000,
+                                              ACCURACY_FLOAT_POINTS_X=0,
+                                              ACCURACY_FLOAT_POINTS_Y=0))
 
-    # result = hillClimbDown(function=sphere_f, domain_x=(-1000, 1000), domain_y=(1, 100), ATTEMPTS=100000,
-    #                        ACCURACY_FLOAT_POINTS_X=1, ACCURACY_FLOAT_POINTS_Y=0)
-    results.append(result)
+    results_sphere_f.append(
+        hillClimbDown_rand(function=sphere_f, domain_x=(-1000, 1000), domain_y=(1, 100), ATTEMPTS=100000,
+                           ACCURACY_FLOAT_POINTS_X=1, ACCURACY_FLOAT_POINTS_Y=0))
 
-for result in results:
-    plt.plot(result)
-plt.show()
+avg_plot(results_mc_cormick_f, 'results_mc_cormick_f RAND')
+avg_plot(results_booth_f, 'results_booth_f RAND')
+avg_plot(results_sphere_f, 'results_sphere_f RAND')
+
+results_mc_cormick_f = []
+results_booth_f = []
+results_sphere_f = []
+for n in range(0, 20):
+    results_mc_cormick_f.append(
+        hillClimbDown(function=mc_cormick_f, domain_x=(-1.5, 4), domain_y=(-3, 4), ATTEMPTS=100000,
+                      ACCURACY_FLOAT_POINTS_X=5, ACCURACY_FLOAT_POINTS_Y=5))
+    results_booth_f.append(
+        hillClimbDown(function=booth_f, domain_x=(-10, 10), domain_y=(-10, 10), ATTEMPTS=1000,
+                      ACCURACY_FLOAT_POINTS_X=0, ACCURACY_FLOAT_POINTS_Y=0))
+
+    results_sphere_f.append(
+        hillClimbDown(function=sphere_f, domain_x=(-1000, 1000), domain_y=(1, 100), ATTEMPTS=100000,
+                      ACCURACY_FLOAT_POINTS_X=1, ACCURACY_FLOAT_POINTS_Y=0))
+
+avg_plot(results_mc_cormick_f, 'results_mc_cormick_f HILLCLIMB')
+avg_plot(results_booth_f, 'results_booth_f HILLCLIMB')
+avg_plot(results_sphere_f, 'results_sphere_f HILLCLIMB')
